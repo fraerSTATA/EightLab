b5138a4f07929166791d069e9047c12e49bc27f5
fraerSTATA <b.ym2002@yandex.ru>
2021-10-16 14:03:43 +0300

    4.0

---
+++ Smirnov/LinearEquation.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smirnov
+{
+    class LinearEquation
+    {
+        protected List<double> x;
+        protected List<double> SolveLinearEquation(double a, double b)// aX+b = 0 -> Linear quation
+        {
+            if (a == 0)
+            {
+                throw new SmirnovException("Определено, что такое уравнение не существует");
+            }
+            SmirnovLog.I().Log("Определено, что это линейное уравнение");
+            return x = new List<double>() { -b / a };
+        }
+    }
+
+}
